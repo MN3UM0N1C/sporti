5acb5486c671df877a13e13e249184f263f0264f
MN3UM0N1C <108032285+MN3UM0N1C@users.noreply.github.com>
2023-12-07 04:16:02 -0800

    Update sport.py

--- sport/sport.py
+++ sport/sport.py
@@ -1,61 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
-import json
 
-user_input = "lionel messi"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
-url = f'https://www.transfermarkt.com/schnellsuche/ergebnis/schnellsuche?query={user_input}'  
-response = requests.get(url, headers=headers)
-data_list = []
+class FootballerScraper:
+    def __init__(self, user_input):
+        self.user_input = user_input
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+        }
+        self.url = f'https://www.transfermarkt.com/schnellsuche/ergebnis/schnellsuche?query={user_input}'
+        self.data_list = []
 
-def dictifier(data):
-    player_info = {}
-    current_key = None
-    for item in data_list:
-        if ':' in item:
-            current_key = item.replace(':', '').strip()
-            player_info[current_key] = None  # Initialize the key with None
-        elif current_key is not None:
-            # Remove unnecessary characters and whitespace
-            value = item.replace('\n', '').strip()
-            # Skip empty values
-            if value:
-                player_info[current_key] = value
-    return player_info
+    def dictifier(self, data):
+        player_info = {}
+        current_key = None
+        for item in data:
+            if ':' in item:
+                current_key = item.replace(':', '').strip()
+                player_info[current_key] = None  # Initialize the key with None
+            elif current_key is not None:
+                # Remove unnecessary characters and whitespace
+                value = item.replace('\n', '').strip()
+                # Skip empty values
+                if value:
+                    player_info[current_key] = value
+        return player_info
 
-def rewards(url):
-    rewards_page = requests.get(url, headers=headers)
-    reward_parsed = BeautifulSoup(rewards_page.text, 'html.parser')
-    
+    def rewards(self, url):
+        rewards_page = requests.get(url, headers=self.headers)
+        reward_parsed = BeautifulSoup(rewards_page.text, 'html.parser')
 
-
-
-def footballer_info(url):
-    classname = "info-table info-table--right-space min-height-audio"
-    page = requests.get(url, headers=headers)
-    info = BeautifulSoup(page.text, 'html.parser')
-    try:
-        div_block = info.find("div", class_="info-table info-table--right-space min-height-audio").find_all("span")
-    except AttributeError:
-        div_block = info.find("div", class_="info-table info-table--right-space").find_all("span")
-    for i in div_block:
-        data_list.append(i.text)
-    return dictifier(data_list) 
-
-
-def search():
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        td_elements = soup.find_all('td', class_="hauptlink")
+    def footballer_info(self, url):
+        classname = "info-table info-table--right-space min-height-audio"
+        page = requests.get(url, headers=self.headers)
+        info = BeautifulSoup(page.text, 'html.parser')
         try:
-            fb_url = "https://www.transfermarkt.com" + td_elements[0].find("a").get("href")
-            return footballer_info(fb_url)
-        except IndexError:
-            return "Footballer not found" 
-    else:
-        return f"Request failed with status code: {response.status_code}"
+            div_block = info.find("div", class_=classname).find_all("span")
+        except AttributeError:
+            div_block = info.find("div", class_=classname.replace(" min-height-audio", "")).find_all("span")
+        for i in div_block:
+            self.data_list.append(i.text)
+        return self.dictifier(self.data_list)
 
-print(search())
+    def search(self):
+        response = requests.get(self.url, headers=self.headers)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            td_elements = soup.find_all('td', class_="hauptlink")
+            try:
+                fb_url = "https://www.transfermarkt.com" + td_elements[0].find("a").get("href")
+                return self.footballer_info(fb_url)
+            except IndexError:
+                return "Footballer not found"
+        else:
+            return f"Request failed with status code: {response.status_code}"
 
-
-
+if __name__ == "__main__":
+    user_input = "cristiano ronaldo"
+    scraper = FootballerScraper(user_input)
+    print(scraper.search())

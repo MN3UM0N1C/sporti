048e35152af9bc6d3b487f464b20d9a1bd9c159c
MN3UM0N1C <108032285+MN3UM0N1C@users.noreply.github.com>
2023-12-11 03:32:45 -0800

    Update sport.py

--- sport/sport.py
+++ sport/sport.py
@@ -1,16 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from fake_useragent import UserAgent
 
 
 class FootballerScraper:
     def __init__(self, user_input):
         self.user_input = user_input
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
+            'User-Agent': UserAgent().random
         }
         self.url = f'https://www.transfermarkt.com/schnellsuche/ergebnis/schnellsuche?query={user_input}'
         self.data_list = []
+        self.proxy_list = requests.get("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=https&timeout=10000&country=all&ssl=all&anonymity=all").text.strip().split('\r\n')
+        self.proxy_to_use = {'https': None}
+        for proxy_string in self.proxy_list:
+            parts = proxy_string.split(':')
+            if len(parts) == 2:
+                ip, port = parts
+                self.proxy_to_use['https'] = f'https://{ip}:{port}'
+            else:
+                print(f"Invalid proxy string: {proxy_string}")
 
     def dictifier(self, data):
         player_info = {}
@@ -31,7 +41,7 @@ class FootballerScraper:
         try:
             rewards_list = []
             rewards_dict = {"rewards" : ""}
-            page = requests.get(f"https://fbref.com/en/search/search.fcgi?hint=neymar&search={self.user_input}", headers=self.headers)
+            page = requests.get(f"https://fbref.com/en/search/search.fcgi?hint=neymar&search={self.user_input}", headers=self.headers, proxies=self.proxy_to_use)
             soup = BeautifulSoup(page.text, 'html.parser')
             for j in soup.find_all("li", {"class" : "important poptip"}):
                 rewards_list.append(j.get_text())
@@ -42,7 +52,7 @@ class FootballerScraper:
 
     def footballer_info(self, url):
         classname = "info-table info-table--right-space min-height-audio"
-        page = requests.get(url, headers=self.headers)
+        page = requests.get(url, headers=self.headers, proxies=self.proxy_to_use)
         info = BeautifulSoup(page.text, 'html.parser')
         try:
             div_block = info.find("div", class_=classname).find_all("span")
@@ -53,7 +63,7 @@ class FootballerScraper:
         return self.dictifier(self.data_list)
 
     def search(self):
-        response = requests.get(self.url, headers=self.headers)
+        response = requests.get(self.url, headers=self.headers, proxies=self.proxy_to_use)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             td_elements = soup.find_all('td', class_="hauptlink")
@@ -69,7 +79,7 @@ class FootballerScraper:
         statistics = {"MP": "", "min": "", "goals":"","assists": ""}
         nums = []
         classname = "tm-player-performance__stats-list-item-value svelte-xwa5ea"
-        page = requests.get(f"https://fbref.com/en/search/search.fcgi?hint=neymar&search={self.user_input}", headers=self.headers)
+        page = requests.get(f"https://fbref.com/en/search/search.fcgi?hint=neymar&search={self.user_input}", headers=self.headers, proxies=self.proxy_to_use)
         info = BeautifulSoup(page.text, 'html.parser')
         try:
             for stats in info.find('div', {'class': 'p1'}).find_all("p"):
@@ -84,7 +94,7 @@ class FootballerScraper:
 
     def table_parser(self, div_id):
         url = f"https://fbref.com/en/search/search.fcgi?&search={self.user_input}"
-        page = requests.get(url, headers=self.headers)
+        page = requests.get(url, headers=self.headers, proxies=self.proxy_to_use)
         info = BeautifulSoup(page.text, 'html.parser')
         table_div = info.find("div", {"id": div_id})
         date_elements = [element.text for element in table_div.find_all("th", {"class" : "left", "data-stat" : "year_id"})]
@@ -96,27 +106,27 @@ class FootballerScraper:
         return json.dumps(result)
 
     def parse_all(self):
-        info = BeautifulSoup(requests.get(f"https://fbref.com/en/search/search.fcgi?&search={self.user_input}", headers=self.headers).text, 'html.parser')
+        info = BeautifulSoup(requests.get(f"https://fbref.com/en/search/search.fcgi?&search={self.user_input}", headers=self.headers, proxies=self.proxy_to_use).text, 'html.parser')
         for t in info.find_all(class_="table_container tabbed current"):
             self.data_list.append(self.table_parser(t.get("id")))
         return self.data_list
 
     def names(self):
-        info = BeautifulSoup(requests.get(f"https://fbref.com/en/search/seArch.fcgi?&sEarch={self.user_input}?", headers=self.headers).text, 'html.parser')
-        print(info)
-        print(info.find_all("div", {"id" : "inpage_nav"}))
+        names = []
+        content = requests.get(f"https://fbref.com/en/search/search.fcgi?&search={self.user_input}", headers=self.headers, proxies=self.proxy_to_use)
+        info = BeautifulSoup(content.text, 'html.parser')
         for k in info.find("div", {"id" : "inpage_nav"}).find_all("li"):
-            self.data_list.append(k.text)
-        del self.data_list[-2:]
-        return json.dumps(dict(zip(self.data_list, self.parse_all())))
-        self.data_list = []
+            names.append(k.text)
+        del names[-2:]
+        return json.dumps(dict(zip(names, self.parse_all())))
+        names = []
 
 
 
 if __name__ == "__main__":
-    user_input = "cristiano+ronaldo"
+    user_input = "lionel+messi"
     scraper = FootballerScraper(user_input)
     #print(scraper.search())
-    print(scraper.search())
+    #print(scraper.search())
     #print(scraper.rewards())
-    #print(scraper.names())
+    print(scraper.names())

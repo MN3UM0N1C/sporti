091f9ee9cf7e59ff2db8e0728fe6482ac3d533d6
MN3UM0N1C <108032285+MN3UM0N1C@users.noreply.github.com>
2024-01-15 04:14:36 -0800

    Update proballers.py

--- proballers.py
+++ proballers.py
@@ -6,75 +6,75 @@ import json
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 basketball_players= [
-        "Payton Pritchard",
-        "Sam Hauser",
-        "Jaylen Brown",
-        "Jayson Tatum",
-        "Derrick White",
-        "Jrue Holiday",
-        "Al Horford",
-        "Kristaps Porzingis",
-        "Luke Kornet",
-        "Oshae Brissett",
-        "Daniel Hackett",
-        "Devontae Cacok",
-        "Iffe Lunderberg",
-        "Isaïa Cordinier",
-        "Jaleen Smith",
-        "Jordan Mickey",
-        "Marco Belinelli",
-        "Ognjen Dobric",
-        "Rihards Lomazs",
-        "Tornike Shengelia",
-        "Gabe Olaseni",
-        "Jordan Taylor",
-        "Joshua Sharma",
-        "Kareem Queeley",
-        "Luke Nelson",
-        "Matt Morgan",
-        "Morayo Soluade",
-        "Rokas Gustys",
-        "Sam Dekker",
-        "Tarik Phillip",
-        'Kevin Durant',
-        'lebron james',
-        'Giannis Antetokounmpo',
-    ]
+		"Payton Pritchard",
+		"Sam Hauser",
+		"Jaylen Brown",
+		"Jayson Tatum",
+		"Derrick White",
+		"Jrue Holiday",
+		"Al Horford",
+		"Kristaps Porzingis",
+		"Luke Kornet",
+		"Oshae Brissett",
+		"Daniel Hackett",
+		"Devontae Cacok",
+		"Iffe Lunderberg",
+		"Isaïa Cordinier",
+		"Jaleen Smith",
+		"Jordan Mickey",
+		"Marco Belinelli",
+		"Ognjen Dobric",
+		"Rihards Lomazs",
+		"Tornike Shengelia",
+		"Gabe Olaseni",
+		"Jordan Taylor",
+		"Joshua Sharma",
+		"Kareem Queeley",
+		"Luke Nelson",
+		"Matt Morgan",
+		"Morayo Soluade",
+		"Rokas Gustys",
+		"Sam Dekker",
+		"Tarik Phillip",
+		'Kevin Durant',
+		'lebron james',
+		'Giannis Antetokounmpo',
+	]
 
 headers = {
-    'Host': 'www.proballers.com',
-    'Sec-Ch-Ua-Mobile': '?0',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.134 Safari/537.36',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Accept': '*/*',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Sec-Ch-Ua-Platform': '""',
-    'Origin': 'https://www.proballers.com',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Dest': 'empty',
-    'Referer': 'https://www.proballers.com/',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Connection': 'close',
+	'Host': 'www.proballers.com',
+	'Sec-Ch-Ua-Mobile': '?0',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.134 Safari/537.36',
+	'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	'Accept': '*/*',
+	'X-Requested-With': 'XMLHttpRequest',
+	'Sec-Ch-Ua-Platform': '""',
+	'Origin': 'https://www.proballers.com',
+	'Sec-Fetch-Site': 'same-origin',
+	'Sec-Fetch-Mode': 'cors',
+	'Sec-Fetch-Dest': 'empty',
+	'Referer': 'https://www.proballers.com/',
+	'Accept-Language': 'en-US,en;q=0.9',
+	'Connection': 'close',
 }
 
 class BasketballScraper:
 	def __init__(self, user_input, team=False):
 		self.headers = {
-	    'Host': 'www.proballers.com',
-	    'Sec-Ch-Ua-Mobile': '?0',
-	    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.134 Safari/537.36',
-	    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'Accept': '*/*',
-	    'X-Requested-With': 'XMLHttpRequest',
-	    'Sec-Ch-Ua-Platform': '""',
-	    'Origin': 'https://www.proballers.com',
-	    'Sec-Fetch-Site': 'same-origin',
-	    'Sec-Fetch-Mode': 'cors',
-	    'Sec-Fetch-Dest': 'empty',
-	    'Referer': 'https://www.proballers.com/',
-	    'Accept-Language': 'en-US,en;q=0.9',
-	    'Connection': 'close',
+		'Host': 'www.proballers.com',
+		'Sec-Ch-Ua-Mobile': '?0',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.134 Safari/537.36',
+		'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+		'Accept': '*/*',
+		'X-Requested-With': 'XMLHttpRequest',
+		'Sec-Ch-Ua-Platform': '""',
+		'Origin': 'https://www.proballers.com',
+		'Sec-Fetch-Site': 'same-origin',
+		'Sec-Fetch-Mode': 'cors',
+		'Sec-Fetch-Dest': 'empty',
+		'Referer': 'https://www.proballers.com/',
+		'Accept-Language': 'en-US,en;q=0.9',
+		'Connection': 'close',
 		}
 		self.user_input = user_input
 		self.data = {'query': self.user_input} 
@@ -86,11 +86,60 @@ class BasketballScraper:
 		except IndexError:
 			self.page_content = False
 
+	def retain_unique_elements(self, input_list):
+		seen = set()
+		unique_elements = []
+		for elem in input_list:
+			# Convert element to its string representation
+			elem_str = str(elem)
+			if elem_str not in seen:
+				seen.add(elem_str)
+				unique_elements.append(elem)
+		return json.dumps(unique_elements)
+
+	def averages(self):
+		if self.page_content:
+			soup = BeautifulSoup(self.page_content.text, 'html.parser')
+			return json.dumps({title.get_text(): stat.get_text() for title, stat in zip(soup.find_all("span", class_="identity__stats__title"), soup.find_all("span", class_="identity__stats__stat"))})
+
 	def parse_html_table(self):
+	    if self.page_content:
+	        soup = BeautifulSoup(self.page_content.text, 'html.parser')
+	        data_list = []
+	        for table in soup.find_all('table'):
+	            table_data = []
+	            headers = []
+	            thead = table.find('thead')
+	            if thead:
+	                header_row = thead.find('tr')
+	                if header_row:
+	                    headers = [header.text.strip().lower() for header in header_row.find_all(['th', 'td'])]
+	            tbody = table.find('tbody')
+	            if tbody:
+	                rows = tbody.find_all('tr')
+	                for row in rows:
+	                    row_data = {}
+	                    cells = row.find_all(['td', 'th'])
+	                    for i, cell in enumerate(cells):
+	                        if i < len(headers):
+	                            header_text = headers[i]
+	                            cell_text = cell.text.strip()
+	                            link = cell.find('a')
+	                            if link:
+	                                link_text = link.get('href')
+	                                row_data[header_text] = {'text': cell_text, 'link': link_text}
+	                            else:
+	                                row_data[header_text] = cell_text
+	                    table_data.append(row_data)
+	            data_list.append(table_data)
+	        return json.dumps(data_list)
+
+	def parse_euroleague(self):
 		if self.page_content:
 			soup = BeautifulSoup(self.page_content.text, 'html.parser')
 			data_list = []
-			table_body = soup.find_all('tbody')[1]
+			soup2 = soup.find('div', {"aria-labelledby": True, "class" : True, "id" : True})
+			table_body = soup2.find("tbody")
 			rows = table_body.find_all('tr')
 			num_columns = max(len(row.find_all(['td', 'th'])) for row in rows)
 			for row in rows:
@@ -105,12 +154,61 @@ class BasketballScraper:
 						row_data[header_text] = {'text': cell_text, 'link': link_text}
 					else:
 						row_data[header_text] = cell_text
-					data_list.append(row_data)
-			json_data = json.dumps(data_list)
-			return json_data
+				data_list.append(row_data)
+			return self.retain_unique_elements(data_list)
+
+	def parse_games_table(self):
+		if self.page_content:
+			soup = BeautifulSoup(requests.get(f'{self.page_content.url}/games').text, "html.parser")
+			data_list = []
+			for table in soup.find_all('table'):
+				table_data = []
+				headers = []
+				thead = table.find('thead')
+				if thead:
+				    header_row = thead.find('tr')
+				    if header_row:
+				        headers = [header.text.strip().lower() for header in header_row.find_all(['th', 'td'])]
+				tbody = table.find('tbody')
+				if tbody:
+				    rows = tbody.find_all('tr')
+				    for row in rows:
+				        row_data = {}
+				        cells = row.find_all(['td', 'th'])
+				        for i, cell in enumerate(cells):
+				            if i < len(headers):
+				                header_text = headers[i]
+				                cell_text = cell.text.strip()
+				                link = cell.find('a')
+				                if link:
+				                    link_text = link.get('href')
+				                    row_data[header_text] = {'text': cell_text, 'link': link_text}
+				                else:
+				                    row_data[header_text] = cell_text
+				        table_data.append(row_data)
+				data_list.append(table_data)
+				return json.dumps(data_list)
+
+	def leaders(self):
+		if self.page_content:
+			soup = BeautifulSoup(requests.get(f'{self.page_content.url}games').text, "html.parser")
+			data_list = []
+			for leader in soup.find('div', class_='team-leaders__entry-container').find_all("li", {"class": "team-leaders__entry"}):
+				data_list.append(leader.get_text().replace("\n", "").replace("  ", ""))
+			return data_list
 		else:
 			return "Not Found"
 
+
+
 for i in basketball_players:	
-	scraper = BasketballScraper("Boston Celtics", True)
-	print(f'{scraper.parse_html_table()}\n{i}\n\n\n')
+	scraper = BasketballScraper("alba berlin", True)
+	# print(scraper.parse_games_table())
+	# print(scraper.parse_html_table())
+	# print(scraper.averages())
+	print(scraper.parse_euroleague())
+	print(scraper.leaders())
+	break
+
+
+

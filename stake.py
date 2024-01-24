e5adcc328d87a7ff90c390027c128e89e7b4674a
MN3UM0N1C <108032285+MN3UM0N1C@users.noreply.github.com>
2024-01-24 01:52:35 -0800

    Update stake.py

--- stake.py
+++ stake.py
@@ -1,94 +1,72 @@
 from bs4 import BeautifulSoup
 import json
 import requests
-from fake_headers import Headers
+from zenrows import ZenRowsClient
 
-headers = Headers()
+client = ZenRowsClient("c6301ed331b3e1626f37f9d806c6c5ea79c9642c")
 
 class StakeScraper():
     def __init__(self):
-        self.match_list_url = "http://stake.com/sports/soccer"
-        self.headers =  {
-        'Host': 'stake.com',
-        'Cookie': '__cf_bm=yGpWaB5YG600.kWug3OUgofEs1mUTraafJD6Vymsy.c-1705586710-1-AWl1Iw5UeRTwaToYHkr79QttvTMuopKp5cl4xmFEDrM6k8LofIyykEJCmlFnFKzgjVxr1LSbtB1PGgCEy8q+FjA=; cf_clearance=7Ps3rULab_5cWXaZOKciWBBw73d0o7UIKaULk3N7ux0-1705586715-1-ARrqe/415gpxD9NzCjlFDJ9XYgYf4azGwV05Dkgx9MmoEi8wKiuveAYE0KIQpyEZfz8mrATU6B8wgfbeqoKc1Mw=; currency_currency=btc; currency_hideZeroBalances=false; currency_currencyView=crypto; currency_bankingCurrencies=[]; session_info=undefined; fiat_number_format=en; cookie_consent=false; leftSidebarView_v2=expanded; sidebarView=hidden; casinoSearch=["Monopoly","Crazy Time","Sweet Bonanza","Money Train","Reactoonz"]; sportsSearch=["Liverpool FC","Kansas City Chiefs","Los Angeles Lakers","FC Barcelona","FC Bayern Munich"]; sportMarketGroupMap={}; oddsFormat=decimal; locale=en; mp_e29e8d653fb046aa5a7d7b151ecf6f99_mixpanel=%7B%22distinct_id%22%3A%20%22%24device%3A18d1ce4032d2944-0be52ee14f4b428-47380724-1fa400-18d1ce4032d2945%22%2C%22%24device_id%22%3A%20%2218d1ce4032d2944-0be52ee14f4b428-47380724-1fa400-18d1ce4032d2945%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D; _ga=GA1.2.911911304.1705586729; _gid=GA1.2.721321414.1705586729; _ga_TWGX3QNXGG=GS1.1.1705586729.1.1.1705587283.0.0.0; intercom-id-cx1ywgf2=119d43e1-ce04-4ba2-a4dc-57540ff57805; intercom-session-cx1ywgf2=; intercom-device-id-cx1ywgf2=07c8e9cf-1831-47ca-a1bd-2034992de1dc',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate',
-        'Upgrade-Insecure-Requests': '1',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-User': '?1',
-        'Te': 'trailers'
-        }
-
+        self.base_url = "http://stake.com"
+        self.match_list_url = "http://stake.com/sports/soccer/"
+        self.match_name = ""
 
     def get_matches(self):
         links = []
-        match_page = BeautifulSoup(requests.get(self.match_list_url, headers=self.headers, allow_redirects=True).text, 'html.parser')
-        matches = match_page.find_all("a", class_="link variant-link size-md align-left svelte-14e5301")
+        match_page = BeautifulSoup(client.get(self.match_list_url).text, 'html.parser')
+        matches = match_page.find_all("a", {"class":"link variant-link size-md align-left svelte-14e5301", "data-sveltekit-preload-data": "off"})
         for match in matches:
-            links.append(f'{self.match_list_url}{match.get("href")}')
-        return links
+            links.append(f'{self.base_url}{match.get("href")}')
+        return set(links)
 
     def parse_all_matches(self):
         results = []
-        for link in self.get_matches():
-            parser = BeautifulSoup(requests.get(link, headers=self.headers).text, "html.parser")
-            results.append(self.html_to_json(parser.find("div", class_="groups svelte-1xk80tl")))
-        return results
-
+        link_list = self.get_matches()
+        for link in link_list:
+            if "sports/soccer"in link:
+                parser = BeautifulSoup(client.get(link).text, "html.parser")
+                results.append(self.html_to_json(parser))
+            else:
+                continue
+        return json.dumps(results)
 
     def extract_odds_data(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         odds_data = {}
-
-        # Find all elements with class 'outcome'
         outcome_elements = soup.find_all('button', {'class': 'outcome'})
-
         for outcome in outcome_elements:
-            # Extract team/country name
             team_name = outcome.find('span', {'class': 'name'}).text.strip()
-
-            # Extract odds value
             odds_value = outcome.find('span', {'class': 'weight-bold'}).text.strip()
-
-            # Add team name and odds value to the dictionary
             odds_data[team_name] = float(odds_value)
-
         return odds_data
 
     def html_to_json(self, html):
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = html
         json_data = {}
-
-        # Find all div elements with class 'secondary-accordion'
         accordion_elements = soup.find_all('div', {'class': 'secondary-accordion'})
-
         for index, accordion in enumerate(accordion_elements):
-            # Extract the market name
             market_name = accordion.find('span', {'class': 'weight-semibold'}).text.strip()
-
-            # Extract odds data using the previously defined function
             odds_data = self.extract_odds_data(str(accordion))
-
-            # Add market name and odds data to the dictionary
             json_data[f'market_{index + 1}_{market_name}'] = odds_data
+        return json_data
 
-        return json.dumps(json_data, indent=2)
+    def data_searcher(self, teams):
+        with open('odds_data.json', 'r') as file:
+            content = file.read()
+        for element in json.loads(content):
+            try:
+                if teams[1] in list(element.values())[0].keys() and teams[0] in list(element.values())[0].keys():
+                    return json.dumps(element)
+                    break
+            except IndexError:
+                continue
+
 
 
 
 scraper = StakeScraper()
-result_json = scraper.parse_all_matches()
-print(result_json)
+#result_json = scraper.parse_all_matches()
+# with open('odds_data.json', 'w') as file:
+#     file.write(result_json)
+print(scraper.data_searcher(["Republic of Korea", "Malaysia"]))
 
-
-
-
-
-
-# link variant-link size-md align-left svelte-14e5301
-# link variant-link size-md align-left svelte-14e5301
-# link variant-link size-md align-left svelte-14e5301

ab2bb3e3a4a7c85dc92de01295c26b4f616b5404
MN3UM0N1C <108032285+MN3UM0N1C@users.noreply.github.com>
2024-01-25 02:38:44 -0800

    Update stake.py

--- stake.py
+++ stake.py
@@ -8,24 +8,34 @@ client = ZenRowsClient("c6301ed331b3e1626f37f9d806c6c5ea79c9642c")
 class StakeScraper():
     def __init__(self):
         self.base_url = "http://stake.com"
-        self.match_list_url = "http://stake.com/sports/soccer/"
+        self.match_list_url = "http://stake.com/sports/soccer/all"
         self.match_name = ""
-
+        self.user_input = ""
+        self.league_id_mapping = {
+        'https://stake.com/sports/soccer/israel/premier-league': 'Premier League',
+        'https://stake.com/sports/soccer/spain/la-liga': 'La Liga',
+        'https://stake.com/sports/soccer/germany/bundesliga': 'Bundesliga',
+        'https://stake.com/sports/soccer/italy/serie-a': 'Serie A',
+        'https://stake.com/sports/soccer/france/ligue-1': 'Ligue 1',
+        'https://stake.com/sports/soccer/international-clubs/uefa-champions-league': 'UEFA Champions League',
+        'https://stake.com/sports/soccer/international-clubs/uefa-europa-league': 'UEFA Europa League',
+        }
     def get_matches(self):
         links = []
-        match_page = BeautifulSoup(client.get(self.match_list_url).text, 'html.parser')
-        matches = match_page.find_all("a", {"class":"link variant-link size-md align-left svelte-14e5301", "data-sveltekit-preload-data": "off"})
-        for match in matches:
-            links.append(f'{self.base_url}{match.get("href")}')
+        for league in list(self.league_id_mapping.keys()):
+            match_page = BeautifulSoup(client.get(league).text, 'html.parser')
+            matches = match_page.find_all("a", {"class":"link variant-link size-md align-left svelte-14e5301", "data-sveltekit-preload-data": "off"})
+            for match in matches:
+                links.append(f'{self.base_url}{match.get("href")}${self.league_id_mapping[league]}')
         return set(links)
 
     def parse_all_matches(self):
-        results = []
-        link_list = self.get_matches()
-        for link in link_list:
+        results = []     
+        link_dict = {key: value for pair in self.get_matches() for key, value in [pair.split('$')]}
+        for link, league in link_dict.items():
             if "sports/soccer"in link:
                 parser = BeautifulSoup(client.get(link).text, "html.parser")
-                results.append(self.html_to_json(parser))
+                results.append({league : self.html_to_json(parser)})
             else:
                 continue
         return json.dumps(results)
@@ -50,12 +60,12 @@ class StakeScraper():
             json_data[f'market_{index + 1}_{market_name}'] = odds_data
         return json_data
 
-    def data_searcher(self, teams):
+    def data_searcher(self, teams, league):
         with open('odds_data.json', 'r') as file:
             content = file.read()
         for element in json.loads(content):
             try:
-                if teams[1] in list(element.values())[0].keys() and teams[0] in list(element.values())[0].keys():
+                if teams[1] in list(list(list(element.values())[0].values())[0].keys()) and teams[0] in list(list(list(element.values())[0].values())[0].keys()) and league in list(element.keys())[0]:
                     return json.dumps(element)
                     break
             except IndexError:
@@ -63,10 +73,16 @@ class StakeScraper():
 
 
 
-
 scraper = StakeScraper()
-#result_json = scraper.parse_all_matches()
+# result_json = scraper.parse_all_matches()
 # with open('odds_data.json', 'w') as file:
 #     file.write(result_json)
-print(scraper.data_searcher(["Republic of Korea", "Malaysia"]))
+print(scraper.data_searcher(["RCD Mallorca", "Real Betis Balompie"], "La Liga"))
 
+
+
+
+
+# link variant-link size-md align-left svelte-14e5301
+# link variant-link size-md align-left svelte-14e5301
+# link variant-link size-md align-left svelte-14e5301

8a923822269d3a66b20b876ce02c80913d196528
MN3UM0N1C <108032285+MN3UM0N1C@users.noreply.github.com>
2024-02-05 03:08:30 -0800

    Update teamsearcher_backup.py

--- sports_xml/teamsearcher_backup.py
+++ sports_xml/teamsearcher_backup.py
@@ -7,9 +7,9 @@ import shutil
 import time
 import os 
 
-
 class FootballDataParser:
     def __init__(self):
+        # Initialize class variables
         self.base_url = "https://www.goalserve.com/getfeed/401117231212497fb27a08db8de47c17/getodds/soccer?cat=soccer_10&League="
         self.league_id_mapping = {
             '1204': 'Premier_League',
@@ -26,22 +26,31 @@ class FootballDataParser:
         self.old_folder = os.path.join(self.output_folder, "old")
 
     def backup_existing_files(self):
-        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        """
+        Backup existing XML files to the 'old' folder with a timestamp.
+        """
         try:
+            current_time = datetime.now().strftime("%Y-%m-%d")
             if not os.path.exists(self.old_folder):
                 os.makedirs(self.old_folder)
             files = os.listdir(self.output_folder)
             xml_files = [f for f in files if f.endswith('.xml')]
             for xml_file in xml_files:
-                shutil.move(os.path.join(self.output_folder, xml_file), os.path.join(self.old_folder, current_time + "-" + xml_file))
-                print(f"Moved {xml_file} to old folder.")
+                if os.path.exists(os.path.join(self.old_folder, current_time + "-" + xml_file)):
+                    print("No need for backing up yet.")
+                else:
+                    shutil.move(os.path.join(self.output_folder, xml_file), os.path.join(self.old_folder, current_time + "-" + xml_file))
+                    print(f"Moved {xml_file} to old folder.")
         except Exception as e:
             print(f"Error backing up existing files: {e}")
 
     def download_file(self, league_id, output_file):
+        """
+        Download XML file for a specific league ID from the API.
+        """
         try:
             url = f"{self.base_url}{league_id}"
-            current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            current_time = datetime.now().strftime("%Y-%m-%d")
             filename = f"{self.league_id_mapping[league_id]}.xml"
             subprocess.run(['curl', '-o', os.path.join(self.output_folder, filename), url], check=True)
             print(f"File downloaded successfully: {filename}")
@@ -50,11 +59,17 @@ class FootballDataParser:
         time.sleep(5)
 
     def load_data(self, league_id):
+        """
+        Load XML data from a file for a specific league ID.
+        """
         with open(os.path.join(self.output_folder, f"{self.league_id_mapping[league_id]}.xml"), 'r') as file:
             xml_content = file.read()
             return BeautifulSoup(xml_content, 'xml')
 
     def search(self, league_id, match, casino=None):
+        """
+        Search for match data within the XML file for a specific league ID.
+        """
         result = []
         soup = self.load_data(league_id)
         for k in soup.find_all("match", {"id": match}):
@@ -65,11 +80,17 @@ class FootballDataParser:
         return result
 
     def is_match(self, team_name, match_name):
+        """
+        Check if a team name matches the match name.
+        """
         team_words = team_name.lower().split()
         match_words = match_name.lower().split()
         return any(word in match_words for word in team_words)
 
     def team(self, league_id, teams, casino=None):
+        """
+        Find match data for a specific team within the XML file for a league.
+        """
         soup = self.load_data(league_id)
         matches = soup.find_all("match")
         for match in matches:
@@ -82,39 +103,56 @@ class FootballDataParser:
                 return self.search(league_id, match.get("id"), casino=casino)
                 
     def koef(self, file):        
+        """
+        Extract odds data from the XML file and return as JSON.
+        """
         data = [(k.get('name'), k.get('value')) for i in file for k in i.find_all('odd')]
         return json.dumps(dict(data))
 
     def download_all_files(self):
+        """
+        Download XML files for all leagues, backing up existing ones first.
+        """
         self.backup_existing_files()
         for league_id, league_name in self.league_id_mapping.items():
             self.download_file(league_id, f"{league_name}.xml")
             time.sleep(5)
 
+
 class BasketballDataParser:
     def __init__(self):
+        # Initialize class variables
         self.base_url = "http://www.goalserve.com/getfeed/401117231212497fb27a08db8de47c17/getodds/soccer?cat=basket_10&League="
         self.league_id_mapping = {"1046": 'nba', "1278": 'euro_league', "1291": 'euro_cup', "2691": 'ncaa'}
         self.output_folder = "app/odds/basketball/"
         self.old_folder = os.path.join(self.output_folder, "old")
 
     def backup_existing_files(self):
+        """
+        Backup existing XML files to the 'old' folder with a timestamp.
+        """
         try:
-            current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            current_time = datetime.now().strftime("%Y-%m-%d")
             if not os.path.exists(self.old_folder):
                 os.makedirs(self.old_folder)
             files = os.listdir(self.output_folder)
             xml_files = [f for f in files if f.endswith('.xml')]
             for xml_file in xml_files:
-                shutil.move(os.path.join(self.output_folder, xml_file), os.path.join(self.old_folder, current_time + "-" + xml_file))
-                print(f"Moved {xml_file} to old folder.")
+                if os.path.exists(os.path.join(self.old_folder, current_time + "-" + xml_file)):
+                    print("No need for backing up yet.")
+                else:
+                    shutil.move(os.path.join(self.output_folder, xml_file), os.path.join(self.old_folder, current_time + "-" + xml_file))
+                    print(f"Moved {xml_file} to old folder.")
         except Exception as e:
             print(f"Error backing up existing files: {e}")
 
     def download_file(self, league_id, output_file):
+        """
+        Download XML file for a specific league ID from the API.
+        """
         try:
             url = f"{self.base_url}{league_id}"
-            current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            current_time = datetime.now().strftime("%Y-%m-%d")
             filename = f"{self.league_id_mapping[league_id]}.xml"
             subprocess.run(['curl', '-o', os.path.join(self.output_folder, filename), url], check=True)
             print(f"File downloaded successfully: {filename}")
@@ -122,6 +160,9 @@ class BasketballDataParser:
             print(f"Error downloading file: {e}")
 
     def load_data(self, league_id):
+        """
+        Load XML data from a file for a specific league ID.
+        """
         try:
             with open(os.path.join(self.output_folder, f"{self.league_id_mapping[league_id]}.xml"), 'r') as file:
                 xml_content = file.read()
@@ -130,6 +171,9 @@ class BasketballDataParser:
             print(f"Error: File not found for league ID {league_id}. Make sure to download the file first.")
 
     def search(self, league_id, match, casino=None):
+        """
+        Search for match data within the XML file for a specific league ID.
+        """
         result = []
         try:
             soup = self.load_data(league_id)
@@ -143,11 +187,17 @@ class BasketballDataParser:
             print("Error: Data structure is not as expected. Check if the XML file has the correct structure.")
 
     def is_match(self, team_name, match_name):
+        """
+        Check if a team name matches the match name.
+        """
         team_words = team_name.lower().split()
         match_words = match_name.lower().split()
         return any(word in match_words for word in team_words)        
 
     def team(self, league_id, teams, casino=None):
+        """
+        Find match data for a specific team within the XML file for a league.
+        """
         soup = self.load_data(league_id)
         matches = soup.find_all("match")
         for match in matches:
@@ -157,6 +207,9 @@ class BasketballDataParser:
                 return self.search(league_id, match.get("id"), casino=casino)
 
     def koef(self, file):
+        """
+        Extract odds data from the XML file and return as JSON.
+        """
         try:
             data = [(k.get('name'), k.get('value')) for i in file for k in i.find_all('odd')]
             return json.dumps(dict(data))
@@ -164,6 +217,9 @@ class BasketballDataParser:
             print("Coefficients not found.")
 
     def download_all_files(self):
+        """
+        Download XML files for all leagues, backing up existing ones first.
+        """
         self.backup_existing_files()
         for league_id, league_name in self.league_id_mapping.items():
             self.download_file(league_id, f"{league_name}.xml")
@@ -172,28 +228,38 @@ class BasketballDataParser:
 
 class TennisDataScraper:
     def __init__(self):
+        # Initialize class variables
         self.base_url = "http://www.goalserve.com/getfeed/401117231212497fb27a08db8de47c17/getodds/soccer?cat=tennis_10"
         self.output_folder = "app/odds/tennis/"
         self.old_folder = os.path.join(self.output_folder, "old")
 
     def backup_existing_files(self):
-        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        """
+        Backup existing XML files to the 'old' folder with a timestamp.
+        """
         try:
+            current_time = datetime.now().strftime("%Y-%m-%d")
             if not os.path.exists(self.old_folder):
                 os.makedirs(self.old_folder)
             files = os.listdir(self.output_folder)
             xml_files = [f for f in files if f.endswith('.xml')]
             for xml_file in xml_files:
-                shutil.move(os.path.join(self.output_folder, xml_file), os.path.join(self.old_folder, current_time + "-"  + xml_file))
-                print(f"Moved {xml_file} to old folder.")
+                if os.path.exists(os.path.join(self.old_folder, current_time + "-" + xml_file)):
+                    print("No need for backing up yet.")
+                else:
+                    shutil.move(os.path.join(self.output_folder, xml_file), os.path.join(self.old_folder, current_time + "-" + xml_file))
+                    print(f"Moved {xml_file} to old folder.")
         except Exception as e:
             print(f"Error backing up existing files: {e}")
 
     def download_file(self, output_file):
+        """
+        Download XML file for tennis matches.
+        """
         try:
             self.backup_existing_files()
             url = self.base_url
-            current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            current_time = datetime.now().strftime("%Y-%m-%d")
             filename = f"tennis.xml"
             subprocess.run(['curl', '-o', os.path.join(self.output_folder, filename), url], check=True)
             print(f"File downloaded successfully: {filename}")
@@ -201,14 +267,20 @@ class TennisDataScraper:
             print(f"Error downloading file: {e}")
 
     def load_data(self):
+        """
+        Load XML data from the tennis XML file.
+        """
         try:
-            with open(f'data/tennis/tennis.xml', 'r') as file:
+            with open(f'odds/tennis/tennis.xml', 'r') as file:
                 xml_content = file.read()
                 return BeautifulSoup(xml_content, 'xml')
         except FileNotFoundError:
-            print(f"Error: File not found for league ID . Make sure to download the file first.")
+            print(f"Error: File not found for tennis matches. Make sure to download the file first.")
 
     def search(self, match, casino=None):
+        """
+        Search for match data within the tennis XML file.
+        """
         result = []
         try:
             soup = self.load_data()
@@ -222,11 +294,17 @@ class TennisDataScraper:
             print("Error: Data structure is not as expected. Check if the XML file has the correct structure.")
 
     def is_match(self, team_name, match_name):
+        """
+        Check if a team name matches the match name.
+        """
         team_words = team_name.lower().split()
         match_words = match_name.lower().split()
         return any(word in match_words for word in team_words)        
 
     def team(self, teams, casino=None):
+        """
+        Find match data for a specific tennis match.
+        """
         soup = self.load_data()
         matches = soup.find_all("match")
         for match in matches:
@@ -235,38 +313,55 @@ class TennisDataScraper:
                 return self.search(match.get("id"), casino=casino)
 
     def koef(self, file):
+        """
+        Extract odds data from the tennis XML file and return as JSON.
+        """
         try:
             data = [(k.get('name'), k.get('value')) for i in file for k in i.find_all('odd')]
             return json.dumps(dict(data))
         except (TypeError, AttributeError):
-
             print("Coefficients not found.")
 
     def download_all_files(self):
+        """
+        Download the tennis XML file.
+        """
         self.download_file("tennis.xml")
 
 class MMADataParser:
     def __init__(self):
+        # Initialize class variables
         self.base_url = "http://www.goalserve.com/getfeed/401117231212497fb27a08db8de47c17/getodds/soccer?cat=mma_10"
         self.output_folder = "app/odds/mma/"
+        self.old_folder = os.path.join(self.output_folder, "old")
+
     def backup_existing_files(self):
-        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        """
+        Backup existing XML files to the 'old' folder with a timestamp.
+        """
         try:
-            if not os.path.exists(f"{self.output_folder}old"):
-                os.makedirs(f"{self.output_folder}old")
+            current_time = datetime.now().strftime("%Y-%m-%d")
+            if not os.path.exists(self.old_folder):
+                os.makedirs(self.old_folder)
             files = os.listdir(self.output_folder)
             xml_files = [f for f in files if f.endswith('.xml')]
             for xml_file in xml_files:
-                shutil.move(f"{self.output_folder}{xml_file}", f"{self.output_folder}old/{current_time}-{xml_file}")
-                print(f"Moved {xml_file} to old folder.")
+                if os.path.exists(os.path.join(self.old_folder, current_time + "-" + xml_file)):
+                    print("No need for backing up yet.")
+                else:
+                    shutil.move(os.path.join(self.output_folder, xml_file), os.path.join(self.old_folder, current_time + "-" + xml_file))
+                    print(f"Moved {xml_file} to old folder.")
         except Exception as e:
             print(f"Error backing up existing files: {e}")
 
     def download_file(self, output_file):
+        """
+        Download XML file for MMA matches.
+        """
         try:
             self.backup_existing_files()
             url = self.base_url
-            current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            current_time = datetime.now().strftime("%Y-%m-%d")
             filename = f"mma.xml"
             subprocess.run(['curl', '-o', f"{self.output_folder}{filename}", url], check=True)
             print(f"File downloaded successfully: {filename}")
@@ -274,14 +369,20 @@ class MMADataParser:
             print(f"Error downloading file: {e}")
 
     def load_data(self):
+        """
+        Load XML data from the MMA XML file.
+        """
         try:
             with open(f'app/odds/mma/mma.xml', 'r') as file:
                 xml_content = file.read()
                 return BeautifulSoup(xml_content, 'xml')
         except FileNotFoundError:
-            print(f"Error: File not found for league ID . Make sure to download the file first.")
+            print(f"Error: File not found for MMA matches. Make sure to download the file first.")
 
     def search(self, match, casino=None):
+        """
+        Search for match data within the MMA XML file.
+        """
         result = []
         try:
             soup = self.load_data()
@@ -295,11 +396,17 @@ class MMADataParser:
             print("Error: Data structure is not as expected. Check if the XML file has the correct structure.")
 
     def is_match(self, team_name, match_name):
+        """
+        Check if a team name matches the match name.
+        """
         team_words = team_name.lower().split()
         match_words = match_name.lower().split()
         return any(word in match_words for word in team_words)        
 
     def team(self, teams, casino=None):
+        """
+        Find match data for a specific MMA match.
+        """
         soup = self.load_data()
         matches = soup.find_all("match")
         for match in matches:
@@ -309,20 +416,23 @@ class MMADataParser:
                 return self.search(match.get("id"), casino=casino)
 
     def koef(self, file):
+        """
+        Extract odds data from the MMA XML file and return as JSON.
+        """
         try:
             data = [(k.get('name'), k.get('value')) for i in file for k in i.find_all('odd')]
             return json.dumps(dict(data))
         except (TypeError, AttributeError):
-
             print("Coefficients not found.")
-
     def download_all_files(self):
+        """
+        Download the MMA XML file.
+        """
         self.download_file("mma.xml")
 
 if __name__ == "__main__":
+    #basketball_parser = FootballDataParser()
+    #print(basketball_parser.koef(basketball_parser.team("1204", ["Everton", "Tottenham"], casino="bwin")))
+    #basketball_parser.download_all_files()
     mma_parser = MMADataParser()
-    basketball_parser = BasketballDataParser()
-    football_parser = FootballDataParser()
     mma_parser.download_all_files()
-    basketball_parser.download_all_files()
-    football_parser.download_all_files()
